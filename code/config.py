12c52c1245f8112751dfca436c0b837923338707
aelkhate <Ahmed.elkhateeb@valeo.com>
2025-05-07 06:17:14 +0300

    add paths as a configiration in the config.py file

--- code/config.py
+++ code/config.py
@@ -1,9 +1,13 @@
 import os 
-original_data_test_path = "/p/playground/Glioma-mdc2025/data/original_data/test"
-original_data_train_path = "/p/playground/Glioma-mdc2025/data/original_data/train"
-original_data_png_test_path = "/p/playground/Glioma-mdc2025/data/original_data_png/test"
-original_data_png_train_path = "/p/playground/Glioma-mdc2025/data/original_data_png/train"
-stain_normailzed_data_train_path = "/p/playground/Glioma-mdc2025/data/stain_normalized_data/train"
-stain_normailzed_data_test_path = "/p/playground/Glioma-mdc2025/data/stain_normalized_data/test"
-cropped_data_test_path = "/p/playground/Glioma-mdc2025/data/cropped_data/test"
-cropped_data_train_path = "/p/playground/Glioma-mdc2025/data/cropped_data/train"
+original_data_train_path = "../data/original_data/train"
+original_data_test_path = "../data/original_data/test"
+original_data_fixed_train_path = "../data/original_data_fixed/train"
+original_data_fixed_test_path = "../data/original_data_fixed/test"
+preprocessed_data_train_path = "../data/preprocessed_data/train"
+preprocessed_data_test_path = "../data/preprocessed_data/test"
+
+#stain_normailzed_data_train_path = "/p/playground/Glioma-mdc2025/data/stain_normalized_data/train"
+#stain_normailzed_data_test_path = "/p/playground/Glioma-mdc2025/data/stain_normalized_data/test"
+#cropped_data_train_path = "/p/playground/Glioma-mdc2025/data/cropped_data/train"
+#cropped_data_test_path = "/p/playground/Glioma-mdc2025/data/cropped_data/test"
+
